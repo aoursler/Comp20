8c78761993d6762c879c3c9b96b95bc09ad63fa4
aoursler <anneoursler@gmail.com>
2015-10-07 15:35:17 -0400

    Background working

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,16 @@
-// Replace this with your code...
+function init(){
+    var canvas = document.getElementById('game_canvas');
+    var ctx = canvas.getContext('2d');
+    var img = new Image();
+    img.src = "duckhunt-background.gif";
+    var duck1 = new Image();
+    var duck2 = new Image();
+    duck1.src = "duckhunt_various_sheet.png";
+    duck2.src = "duckhunt_various_sheet.png";
+    img.onload = function(){
+	ctx.drawImage(img, 25, 25, 800, 600) 
+	//	ctx.drawImage(duck1,
+    };
+    
+    console.log("Hi!");
+}
