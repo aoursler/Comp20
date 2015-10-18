e4c9389ce5af7510978c5fa09877a9f68ada79c2
aoursler <anneoursler@gmail.com>
2015-10-18 16:18:01 -0400

    XMLHttpRequest()?

--- messages/lab.js
+++ messages/lab.js
@@ -1,5 +1,8 @@
 function parse(){
-    jsondata = '[{"id":7,"app_id":4,"content":"admin","username":"admin","active":true,"created_at":"2015-09-14T19:11:00.471Z","updated_at":"2015-09-14T19:11:00.471Z"},{"id":8,"app_id":1,"content":"Foo","username":"fury","active":true,"created_at":"2015-10-15T16:58:52.451Z","updated_at":"2015-10-15T16:58:52.451Z"},{"id":9,"app_id":1,"content":"Bar","username":"fury","active":true,"created_at":"2015-10-15T16:59:15.289Z","updated_at":"2015-10-15T16:59:15.289Z"}]'//http://messagehub.herokuapp.com/messages.json';
+    var jsondata = 'http://messagehub.herokuapp.com/messages.json';
+    xhr = new XMLHttpRequest();
+    xhr.open("get", "jsondata", true);
+
     parsedObjects = JSON.parse(jsondata);
 
     elem = document.getElementById("messages");
