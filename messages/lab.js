f0ad5dc7a26e24684f3eef14799e38b968c877e2
aoursler <anneoursler@gmail.com>
2015-10-18 15:21:20 -0400

    lab.js filled in on branch

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,9 @@
-// Your JavaScript goes here...
+function parse(){
+    jsondata = "http://messagehub.herokuapp.com/messages.json";
+    parsedOjects = JSON.parse(jsondata);
+    elem = document.getElementById("results");
+    for (count = 0; count < parsedObjects[count]){
+	console.log(Object.keys(parsedObjects[count]));
+	elem.innerHTML += "<p>Data: " + parsedObjects[count] + "</p>";
+    }
+}
