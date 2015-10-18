5e80067642c4b08e387c983833a078e43f0bedf4
aoursler <anneoursler@gmail.com>
2015-10-18 15:25:57 -0400

    lab.js for loop fix

--- messages/lab.js
+++ messages/lab.js
@@ -1,8 +1,10 @@
+parse();
+
 function parse(){
     jsondata = "http://messagehub.herokuapp.com/messages.json";
     parsedOjects = JSON.parse(jsondata);
     elem = document.getElementById("results");
-    for (count = 0; count < parsedObjects[count]){
+    for ( count=0; count<parsedObjects[count]; count++){
 	console.log(Object.keys(parsedObjects[count]));
 	elem.innerHTML += "<p>Data: " + parsedObjects[count] + "</p>";
     }
