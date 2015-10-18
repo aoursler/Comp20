7c14ceebbf00c549437a14ee0e30b48b28b24f5b
aoursler <anneoursler@gmail.com>
2015-10-18 15:35:08 -0400

    id changed to messages

--- messages/lab.js
+++ messages/lab.js
@@ -3,9 +3,12 @@ parse();
 function parse(){
     jsondata = "http://messagehub.herokuapp.com/messages.json";
     parsedOjects = JSON.parse(jsondata);
-    elem = document.getElementById("results");
+
+    elem = document.getElementById("messages");
+
     for ( count=0; count<parsedObjects[count]; count++){
 	console.log(Object.keys(parsedObjects[count]));
 	elem.innerHTML += "<p>Data: " + parsedObjects[count] + "</p>";
     }
+
 }
