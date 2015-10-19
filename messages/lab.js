b81f660ebbd58ae7d3eb9cd641abf7264790395b
aoursler <anneoursler@gmail.com>
2015-10-19 12:29:35 -0400

    changes wrongly made to master

--- messages/lab.js
+++ messages/lab.js
@@ -1,7 +1,19 @@
 function parse(){
-    jsondata = "http://messagehub.herokuapp.com/messages.json";
-    parsedOjects = JSON.parse(jsondata);
-    elem = document.getElementById("results");
+ 
+    var Request = new XMLHttpRequest();
+    var jsondata = "http://messagehub.herokuapp.com/messages.json";
+    Request.open("GET", jsondata, true);
+
+    Request.onreadystatechange = function(){
+	if (Request.readyState == 4 && Request.status == 200){
+  
+	}
+    }
+
+    Request.send();
+
+    var parsedOjects = JSON.parse(jsondata);
+    var elem = document.getElementById("results");
     for (count = 0; count < parsedObjects[count]){
 	console.log(Object.keys(parsedObjects[count]));
 	elem.innerHTML += "<p>Data: " + parsedObjects[count] + "</p>";
